3ca522f5929855856f5825d72e1d954d83b91967
justinmavity <justinmavity1021@gmail.com>
2023-03-18 00:01:34 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import TodoList from "./components/TodoComponents/TodoList";
-import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
 
 class App extends React.Component {
   constructor() {
